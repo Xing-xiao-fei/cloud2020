1191abb8462634ceb0387faa335f85336aba9f84
15263171755 <Zhoupeng590831>
2021-08-19 15:37:40 +0800

    pushpush github测试push

--- cloud-provider-hygtrix-payment8001/src/main/java/com/xxf/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hygtrix-payment8001/src/main/java/com/xxf/springcloud/PaymentHystrixMain8001.java
@@ -23,8 +23,8 @@ public class PaymentHystrixMain8001 {
     /**
      * 此配置是为了服务监控而配置，与服务容错本身无关，springcloud升级后的坑
      * ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"，
-     * 只要在自己的项目里配置上下面的servlet就可以了
-     * 否则，Unable to connect to Command Metric Stream 404
+     * 只要在自己的项目里配置上下面的servlet就可以了 pushpush github测试push
+     * 否则 ，Unable to connect to Command Metric Stream 404
      */
     @Bean
     public ServletRegistrationBean getServlet() {
